648ac4075b7d7fae2b94c2de7bbacec873a6265d
gagan <gagandeepkaur8@academic.rrc.ca>
2023-07-17 15:55:10 -0500

    dropdown changed

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -12,7 +12,7 @@ interface DropdownProps {
 const DropdownWrapper = styled.select<DropdownProps>`
   /* Default styles */
   padding: 10px;
-  border: 2px;
+  border: 20px;
   cursor: pointer;
 
   /* Background color */
